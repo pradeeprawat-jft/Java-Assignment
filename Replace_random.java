60f8bed856eb7f54f0503a27f93cdda384b9c050
pradeep rawat <pradeep.rawat@jellyfishtechnologies.com>
2023-03-22 13:33:40 +0530

    Approach change for Replace_random

--- Replace_random.java
+++ Replace_random.java
@@ -4,35 +4,29 @@ public class Replace_random {
     int make_zero(int rand) {
         int sum = 0;
         for (int i = 0; i < 10; i++) {
-            sum+=arr[i];
-            if (i == rand) {
+            sum += arr[i];
+            if (i == rand)
                 arr[i] = 0;
-            }
         }
-        for (int i = 0; i<10;i++)
-        {
-            sum-=arr[i];
-        }
+        for (int i = 0; i < 10; i++)
+            sum -= arr[i];
         return sum;
     }
-    void printarr() {
+    void printArr() {
         System.out.println("After Array is ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(arr[i]+" ");
-        }
+        for (int i = 0; i < 10; i++)
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
     public static void main(String[] args) {
         Replace_random obj = new Replace_random();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 10 Element into the array form 1 - 10");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
             obj.arr[i] = sc.nextInt();
-        }
         int rand = (int) (Math.random() * 10)+1;
         int num = obj.make_zero(rand);
-        obj.printarr();
+        obj.printArr();
         System.out.println("Replaced Element is " + num);
-
     }
 }
